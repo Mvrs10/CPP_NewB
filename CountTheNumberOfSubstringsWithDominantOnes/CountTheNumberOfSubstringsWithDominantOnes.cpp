d4e740f59e23f991cf211580813aee7c8dd19b12
Minh Khoi Phan <pmka42@gmail.com>
2025-11-16 12:14:08 -0500

    Count the Number of Substrings With Dominant Ones.

---
+++ CountTheNumberOfSubstringsWithDominantOnes/CountTheNumberOfSubstringsWithDominantOnes.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "SubstringDominantOnes.h"
+
+int main()
+{
+    std::string s = "101101";
+    int ans = numberOfStrings(s);
+    std::cout << ans << std::endl;
+}
---
+++ CountTheNumberOfSubstringsWithDominantOnes/SubstringDominantOnes.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <algorithm>
+
+static int numberOfStrings(std::string s) {
+    int n = s.length();
+    std::vector<int> pre(n + 1);
+    pre[0] = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || (i > 0 && s[i - 1] == '0')) {
+            pre[i + 1] = i;
+        }
+        else {
+            pre[i + 1] = pre[i];
+        }
+    }
+
+    int result = 0;
+    for (int i = 1; i <= n; i++) {
+        int cnt0 = (s[i - 1] == '0') ? 1 : 0;
+        int j = i;
+
+        while (j > 0 && cnt0 * cnt0 <= n) {
+            int cnt1 = (i - pre[j]) - cnt0;
+
+            if (cnt0 * cnt0 <= cnt1) {
+                result += std::min(j - pre[j], cnt1 - cnt0 * cnt0 + 1);
+            }
+
+            j = pre[j];
+            cnt0++;
+        }
+    }
+
+    return result;
+}
